579ec4ebc09d79e04651c283cf442aa840ba269f
khanhphantt <85212452+khanhphantt@users.noreply.github.com>
2022-06-28 17:48:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,9 @@ def mask_image():
             # ensure the bounding boxes fall within the dimensions of
             # the frame
             if startX > w:
-				continue
-			if startY > h:
-				continue
+                continue
+            if startY > h:
+                continue
             (startX, startY) = (max(0, startX), max(0, startY))
             (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
